ca944eb175b53b73ecaa99e76b91fcf8b1d4ccac
Maciej <mwkm00@gmail.com>
2023-03-12 12:14:37 +0100

    initial commit

---
+++ utils.py
@@ -0,0 +1,16 @@
+from flask import session, redirect
+from functools import wraps
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        user = dict(session).get('id', None)
+        if user:
+            return f(*args, **kwargs)
+        return redirect("/login")
+    return decorated_function
+
+def allowed_ext(filename):
+    file_ext = filename.rsplit('.', 1)[1].lower()
+    ALLOWED_EXTENSIONS = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']
+    return file_ext in ALLOWED_EXTENSIONS
